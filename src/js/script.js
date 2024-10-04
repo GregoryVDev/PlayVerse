f6201390dfc96a4a7c6367cf3c47b05255f303fd
GregoryVDev <arexo@hotmail.fr>
2024-10-04 10:55:50 +0200

    Created the navbar with the burger menu and end of favourites page by adding pagination. Added game images

---
+++ src/js/script.js
@@ -0,0 +1,90 @@
+// MENU BURGER
+
+let burgerMenu = document.getElementById("burger-menu");
+let overlay = document.getElementById("nav");
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+// PAGINATION FAVORIS
+
+document.addEventListener("DOMContentLoaded", function () {
+  const articlesPerPage = 3; // On affiche une limitation de 3 articles par page
+  const articles = document.querySelectorAll(".container-favoris .card"); // Séléction des class container-favoris et card et on récupère les articles à l'intérieur
+  const totalPages = Math.ceil(articles.length / articlesPerPage); // Calcule le nombre de pages en divisant le nombre total d'articles et l'arrondir supérieurement pour avoir assez de page pour les articles
+  let currentPage = 1; // Page par défaut
+
+  function showPage(page) {
+    const start = (page - 1) * articlesPerPage; // L'index commencera à 0 donc on aura les index 0,1,2 sur la page 1
+    const end = start + articlesPerPage; // On va afficher les index 3,4,5
+
+    // On parcourt chaques articles dans la liste
+    articles.forEach((article, index) => {
+      if (index >= start && index < end) {
+        article.style.display = "block"; // Permet d'afficher les 3 articles
+      } else {
+        article.style.display = "none"; // Permet de cacher les autres articles
+      }
+    });
+
+    const prevPageButton = document.getElementById("prevPage");
+
+    if (page === 1) {
+      prevPageButton.style.display = "none";
+    } else {
+      prevPageButton.style.display = "flex";
+      prevPageButton.style.gap = "5px";
+    }
+
+    const nextPageButton = document.getElementById("nextPage");
+
+    if (page === totalPages) {
+      nextPageButton.style.display = "none";
+    } else {
+      nextPageButton.style.display = "flex";
+      nextPageButton.style.gap = "5px";
+    }
+
+    const pageNumbers = document.getElementById("pageNumbers");
+    pageNumbers.innerHTML = ""; // Vide l'élément du html pour remettre à jour quand on change de page
+
+    for (let i = 1; i <= totalPages; i++) {
+      // On rajoute une page jusqu'à atteindre la page demandée
+      const pageNumber = document.createElement("span");
+      pageNumber.textContent = i; // Mise à jour de "i"
+      pageNumber.className = "page-number";
+      pageNumber.style.cursor = "pointer";
+
+      if (i === currentPage) {
+        pageNumber.style.fontWeight = "bold"; // On ajoute en gras la page où on se retrouve
+      }
+
+      pageNumber.addEventListener("click", () => {
+        currentPage = i; // On met l'index à jour pour savoir la page actuelle
+        showPage(currentPage);
+      });
+      pageNumbers.appendChild(pageNumber);
+
+      if (i < totalPages) {
+        pageNumbers.appendChild(document.createTextNode(" ")); // Ajout d'un espace entre les chiffres tant que totalPages est supérieur à la page actuelle
+      }
+    }
+  }
+
+  document.getElementById("prevPage").addEventListener("click", function () {
+    if (currentPage > 1) {
+      currentPage--;
+      showPage(currentPage);
+    }
+  });
+
+  document.getElementById("nextPage").addEventListener("click", function () {
+    if (currentPage < totalPages) {
+      currentPage++;
+      showPage(currentPage);
+    }
+  });
+
+  showPage(currentPage);
+});
---
+++ src/template/navbar.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="./navbar.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="container-left">
+            <div class="logo">
+                <img src="../img/logos/playverse.png" alt="Logo">
+            </div>
+            <div class="container-search">
+                <img src="../img/logos/search.svg" alt="Search">
+                <input type="search" name="search" id="search" placeholder="Cherchez un jeu...">
+            </div>
+        </div>
+        <div id="burger-menu">
+            <span></span>
+        </div>
+        <nav id="nav">
+            <ul class="nav_list">
+                <li><a href="../index.php">Accueil</a></li>
+                <li><a href="../games.php">Jeux</a></li>
+                <li><a href="../reviews.php">Reviews</a></li>
+                <li><a href="../connexion.php" class="button-connect">Connexion</a></li>
+                <li><a href="../inscription.php" class="button-inscri">Inscription</a></li>
+            </ul>
+            <div class="container-buttons">
+                <a href="../connexion.php" class="button-connection">Connexion</a>
+                <a href="../inscription.php" class="button-inscription">Inscription</a>
+            </div>
+        </nav>
+    </header>
+</body>
+<script src="../js/script.js"></script>
+
+</html>
