e98486028bccbcf6ae2eef3ea95dcc022d872b28
GregoryVDev <arexo@hotmail.fr>
2024-10-17 11:59:24 +0200

    Finished reviews page, added photos and logos

--- src/js/script.js
+++ src/js/script.js
@@ -50,11 +50,13 @@ window.onclick = function (event) {
   }
 };
 
-// PAGINATION FAVORIS
+// PAGINATION
 
 document.addEventListener("DOMContentLoaded", function () {
-  const articlesPerPage = 3; // On affiche une limitation de 3 articles par page
-  const articles = document.querySelectorAll(".container-favoris .card"); // Séléction des class container-favoris et card et on récupère les articles à l'intérieur
+  const articlesPerPage = 15; // On affiche une limitation de 3 articles par page
+  const articles = document.querySelectorAll(
+    ".container-favoris .card, .container-reviews .review"
+  ); // Séléction des class container-favoris et card et on récupère les articles à l'intérieur
   const totalPages = Math.ceil(articles.length / articlesPerPage); // Calcule le nombre de pages en divisant le nombre total d'articles et l'arrondir supérieurement pour avoir assez de page pour les articles
   let currentPage = 1; // Page par défaut
 
@@ -111,6 +113,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (i < totalPages) {
         pageNumbers.appendChild(document.createTextNode(" ")); // Ajout d'un espace entre les chiffres tant que totalPages est supérieur à la page actuelle
+      }
+    }
+    if (totalPages <= 1) {
+      document.getElementById("pagination").style.display = "none"; // Cache la pagination entière s'il n'y a qu'une seule page
+    } else {
+      document.getElementById("pagination").style.display = "flex"; // Affiche la pagination si plus d'une page
+
+      if (page === 1) {
+        prevPageButton.style.display = "none";
+      } else {
+        prevPageButton.style.display = "flex";
+        prevPageButton.style.gap = "5px";
+      }
+
+      if (page === totalPages) {
+        nextPageButton.style.display = "none";
+      } else {
+        nextPageButton.style.display = "flex";
+        nextPageButton.style.gap = "5px";
       }
     }
   }
--- src/reviews.php
+++ src/reviews.php
@@ -1,5 +1,15 @@
 <?php
 session_start();
+
+require_once("./connect.php");
+
+
+$sql = $db->prepare("SELECT * FROM reviews ORDER BY review_id DESC");
+
+$sql->execute();
+
+$reviews = $sql->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <?php include "./template/navbar.php" ?>
@@ -7,23 +17,27 @@ session_start();
     <section class="review-section">
         <h1>Les dernières reviews</h1>
         <div class="container-reviews">
-            <article class="review">
-                <figure>
-                    <img src="./img/images/zelda.png" alt="Zelda">
-                    <figcaption>
-                        <h2>The Legend of Zelda</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.
-                            Ea nemo possimus reprehenderit quam, aut amet similique deserunt. Numquam at incidunt, maxime velit vitae deserunt expedita. Recusandae dolore ducimus aliquam iste, corrupti quis, ab, in esse placeat necessitatibus magnam.</p>
-                    </figcaption>
-                </figure>
-            </article>
-            <article class="review">
+            <?php foreach ($reviews as $review) { ?>
+
+                <article class="review">
+                    <a href="./review.php?id=<?= $review["review_id"] ?>">
+                        <figure>
+                            <img src="./img/images/<?= $review["image1"] ?>" alt="<?= $review["review_title"] ?>">
+                            <figcaption>
+                                <h2><?= $review["review_title"] ?></h2>
+                                <p><?= substr($review["paragraph1"], 0, 250) . "..."; ?></p>
+                            </figcaption>
+                        </figure>
+                    </a>
+                </article>
+
+            <?php } ?>
+            <!-- <article class="review">
                 <figure>
                     <img src="./img/images/bmw.png" alt="Black Myth Wukong">
                     <figcaption>
                         <h2>Black Myth Wukong</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.
-                            Ea nemo possimus reprehenderit quam, aut amet similique deserunt. Numquam at incidunt, maxime velit vitae deserunt expedita. Recusandae dolore ducimus aliquam iste, corrupti quis, ab, in esse placeat necessitatibus magnam.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.</p>
                     </figcaption>
                 </figure>
             </article>
@@ -32,8 +46,7 @@ session_start();
                     <img src="./img/images/frostpunk.png" alt="Frostpunk">
                     <figcaption>
                         <h2>Frostpunk 2</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.
-                            Ea nemo possimus reprehenderit quam, aut amet similique deserunt. Numquam at incidunt, maxime velit vitae deserunt expedita. Recusandae dolore ducimus aliquam iste, corrupti quis, ab, in esse placeat necessitatibus magnam.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.</p>
                     </figcaption>
                 </figure>
             </article>
@@ -42,12 +55,22 @@ session_start();
                     <img src="./img/images/sims.png" alt="Les sims">
                     <figcaption>
                         <h2>Les Sims 4</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.
-                            Ea nemo possimus reprehenderit quam, aut amet similique deserunt. Numquam at incidunt, maxime velit vitae deserunt expedita. Recusandae dolore ducimus aliquam iste, corrupti quis, ab, in esse placeat necessitatibus magnam.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime aperiam rerum, tenetur enim eum ullam ratione hic esse placeat rem est modi quos atque veniam! Nulla illo culpa mollitia omnis.</p>
                     </figcaption>
                 </figure>
-            </article>
+            </article> -->
         </div>
     </section>
+    <div id="pagination" class="container-pages">
+        <a id="prevPage" href="#pagination">
+            <img src="./img/logos/angles-left.svg" alt="Page précédente">
+            Précédente
+        </a>
+        <span id="pageNumbers"></span>
+        <a id="nextPage" href="#pagination">
+            Suivante
+            <img src="./img/logos/angles-right.svg" alt="Page suivante">
+        </a>
+    </div>
 </main>
 <?php include "./template/footer.php" ?>
